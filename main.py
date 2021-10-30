0d8e0eccadc522dbf4428afd61db407b48581061
JaredP45 <synetos@protonmail.com>
2021-10-30 14:22:48 -0500

    Scraped information from data variable.

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ import yfinance as yf
 data = yf.download(tickers='TSLA', period='5d',
                    interval='15m', round=True)
 
-print(data)
+fig = go.figure()
+
+fig.add_trace(go.Candlestick(x=data.index, open=data['Open'], high=data['High'],
+                             low=data['Low'], close=data['Close'], name='market data'))
+
+fig.update_layout(title='Tesla Share Price', yaxis_title='Stock Price (USD)')
+
+
 
