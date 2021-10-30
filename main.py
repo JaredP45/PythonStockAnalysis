4ab593c2142c99162c34a95f786427386a968b60
JaredP45 <synetos@protonmail.com>
2021-10-30 14:19:34 -0500

    Changing graphing library from Matplotlib to Plotly; Supplying data sets from S&P 500 CSV file to Yahoo Finance API to expand the usage of the application.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
-import chart
-import loop
-
-if __name__ == '__main__':
-    chart.showChart()
+import plotly.graph_objs as go
+import yfinance as yf
 
 
+data = yf.download(tickers='TSLA', period='5d',
+                   interval='15m', round=True)
+
+print(data)
 
