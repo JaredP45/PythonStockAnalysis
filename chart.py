a493a9eec0607baee54ece77dbb4428d1aae8468
JaredP45 <synetos@protonmail.com>
2021-11-14 17:10:07 -0600

    Implemented looped feedback for adding new charts if the user wishes to look at other stocks. General update.

--- chart.py
+++ chart.py
@@ -8,18 +8,24 @@ class AnalysisChart:
         self.charting = charting
 
     def showData(self):
+        """
+        Loads the data for a period of 1 year, then sets the figure and traces based on which is chosen. Then it sets
+            the title ticks, and shows the entire chart.
+        """
 
-        data = yf.download(tickers=self.choice, period='30d',
+        data = yf.download(tickers=self.choice, period='360d',
                            rounding=True)
 
         fig = go.Figure()
 
         if self.charting == 1:
             fig.add_trace(go.Ohlc(x=data.index, open=data['Open'], high=data['High'],
-                                         low=data['Low'], close=data['Close'], name='market data'))
+                                  low=data['Low'], close=data['Close'], name='market data'))
         elif self.charting == 2:
             fig.add_trace(go.Candlestick(x=data.index, open=data['Open'], high=data['High'],
-                                  low=data['Low'], close=data['Close'], name='market data'))
+                                         low=data['Low'], close=data['Close'], name='market data'))
+        else:
+            print('Charting option does not exist.')
 
         fig.update_layout(title=self.choice + ' Share Price', yaxis_title='Stock Price (USD)')
 
@@ -27,10 +33,10 @@ class AnalysisChart:
             rangeslider_visible=True,
             rangeselector=dict(
                 buttons=list([
-                    dict(count=60, label='60h', step='hour', stepmode='backward'),
-                    dict(count=120, label='120h', step='hour', stepmode='backward'),
-                    dict(count=480, label='480h', step='hour', stepmode='backward'),
-                    dict(count=720, label='720h', step='hour', stepmode='backward'),
+                    dict(count=5, label='5 Days', step='day', stepmode='backward'),
+                    dict(count=15, label='15 Days', step='day', stepmode='backward'),
+                    dict(count=30, label='1 Month', step='day', stepmode='backward'),
+                    dict(count=60, label='2 Months', step='day', stepmode='backward'),
                     dict(step='all')
                 ])
             )
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from chart import AnalysisChart as Ac
 
 if __name__ == '__main__':
-    choice = input("Write a stock symbol: ")
-    charting_style = int(input('Do you want OHLC or Candlestick charting? (1/2)\n>>> '))
+    end_program = False
 
-    Ac.showData(Ac(choice, charting_style))
+    while end_program is not True:
+
+        choice = input("Write a stock symbol: ")
+        charting_style = int(input('Do you want OHLC or Candlestick charting? (1/2)\n>>> '))
+
+        Ac.showData(Ac(choice, charting_style))
+
+        option = input('Do you want to change stocks? (Y/N)\n>>> ')
+
+        if option[0] == 'Y' or option[0] == 'y':
+            end_program = False
+        elif option[0] == 'N' or option[0] == 'n':
+            end_program = True
+        else:
+            print('An error has occurred.')
+            end_program = True
